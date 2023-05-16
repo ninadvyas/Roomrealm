3e30f503077dcff64dca3895c1295d5958c1988d
Ninad Vyas <115214615+NinadVyas@users.noreply.github.com>
2023-05-16 17:05:08 +0530

    third chat

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import {
 import { useEffect, useRef, useState } from "react";
 import { app } from "./firebase";
 import Message from "./Components/Message";
+import Lottie from 'lottie-react'
+import animationData from './Components/artychat.json'
 import {
   Card,
   CardHeader,
@@ -139,23 +141,12 @@ function App() {
           </VStack>
         </Container>
       ) : (
-        <VStack h={"100vh"} bg={"white"} justifyContent={"center"}>
+        <VStack h={"100vh"} bg={"black"} justifyContent={"center"}>
           <Card maxW="sm">
             <CardBody>
-              <Image
-                src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-                alt="Green double couch with wooden legs"
-                borderRadius="lg"
-              />
+            <Lottie width='20%' animationData={animationData}/>
+
               <Stack mt="6" spacing="3" >
-                <center>
-                <Heading size="md" fontSize="3xl">Roomrealm</Heading>
-                <Text >
-                App provides an interactive space where users can connect with others and 
-                engage in meaningful conversations on a wide range of topics, from hobbies and interests 
-                to news and current events.
-                </Text>
-                </center>
               </Stack>
             </CardBody>
             <Divider />
